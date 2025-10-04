6859f4e920fd7fb19e538e2b274f7f8a6a4997d3
Hubert Łojek <huberty89@gmail.com>
2025-10-04 16:47:39 +0200

    Better bug reproducibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useRef, useState } from "react";
 import Item from "./Item.tsx";
 
 const bigData = new Uint8Array(100_000_000);
 
 function App() {
+  const bigDataRef = useRef<Uint8Array<ArrayBuffer> | null>(null);
+
+  const [visible, setVisible] = useState<boolean>(false);
+  const [, setNop] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (visible) {
+      bigDataRef.current = bigData;
+      setNop((prev) => !prev); // force re-render
+    }
+  }, [visible]);
+
   return (
     <>
-      {Array.from({ length: 100 }).map((_, index) => (
-        <Item key={index} data={{ buffer: bigData }} />
-      ))}
+      <button onClick={() => setVisible((prev) => !prev)}>Go</button>
+      {visible &&
+        Array.from({ length: 100 }).map((_, index) => (
+          <Item key={index} data={{ buffer: bigDataRef.current }} />
+        ))}
     </>
   );
 }
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,10 +1,10 @@
 // this needs to be nested, when Uint8Array is used directly in props it's working fine
 interface Data {
-  buffer: Uint8Array<ArrayBuffer>;
+  buffer: Uint8Array<ArrayBuffer> | null;
 }
 
 function Item({ data }: { data: Data }) {
-  return <div>{data.buffer.length}</div>;
+  return <div>{data.buffer?.length}</div>;
 }
 
 export default Item;
